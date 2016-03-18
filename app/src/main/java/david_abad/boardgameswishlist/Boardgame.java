29cedaa472fe176c8f3e414ff8deb4b430f3aa55
David Abad Vich <davidabad10@gmail.com>
2016-03-18 13:44:23 +0100

    Add constructors for class Boardgame

--- app/src/main/java/david_abad/boardgameswishlist/Boardgame.java
+++ app/src/main/java/david_abad/boardgameswishlist/Boardgame.java
@@ -1,5 +1,8 @@
 package david_abad.boardgameswishlist;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by david on 18/03/2016.
  */
@@ -7,7 +10,7 @@ public class Boardgame {
     private String name;
     private int wish_level;
     private float score;
-    private String[] categories;
+    private List<String> categories;
     private int min_players;
     private boolean own;
 
@@ -18,7 +21,33 @@ public class Boardgame {
      * @param _wish_level
      */
     public Boardgame(String _name, int _wish_level) {
-        
+        name = _name;
+        wish_level = _wish_level;
+        score = 0;
+        categories = null;
+        min_players = 0;
+        own = false;
+    }
+
+    /**
+     * Constructor providing all the information for a boardgame.
+     *
+     * @param _name
+     * @param _wish_level
+     * @param _score
+     * @param _categories
+     * @param _min_players
+     */
+    public Boardgame(String _name, int _wish_level, float _score, String _categories, int _min_players) {
+        name = _name;
+        wish_level = _wish_level;
+        score = _score;
+        min_players = _min_players;
+        own = false;
+
+        // Extract the categories
+        categories = Arrays.asList(_categories.split(","));
+
     }
 
     /**
@@ -32,6 +61,14 @@ public class Boardgame {
      * @param _own
      */
     public Boardgame(String _name, int _wish_level, float _score, String _categories, int _min_players, boolean _own) {
+        name = _name;
+        wish_level = _wish_level;
+        score = _score;
+        min_players = _min_players;
+        own = _own;
 
+        // Extract the categories
+        categories = Arrays.asList(_categories.split(","));
     }
+
 }
